ef78fbe8d832cd870f7203e12bf14da59872db8c
Kevin-christ <141647934+Kevin-christ@users.noreply.github.com>
2023-10-27 10:45:49 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,17 +1,38 @@
-function tocasom(seletoraudio){
-const elemento"document.queryselector(selectoraudio);
-  if(elements&& elemento localName= = ="audio"){
-  elemento play();
+function tocaSom (seletorAudio) {
+    const elemento = document.querySelector(seletorAudio);
+
+    if (elemento && elemento.localName === 'audio') {
+        elemento.play();
+    }
+    else {
+        //alert('Elemento não encontrado');
+        console.log('Elemento não encontrado ou seletor inválido');
+    }
+
 }
-  else {
-    console log("Elemento não encontrado ou seletor inválido");
-  }
+
+const listaDeTeclas = document.querySelectorAll('.tecla');
+//para
+for (let contador = 0; contador < listaDeTeclas.length; contador++) {
+
+    const tecla = listaDeTeclas[contador];
+    const instrumento = tecla.classList[1];
+    const idAudio = `#som_${instrumento}`; //template string
+
+    tecla.onclick = function () {
+        tocaSom(idAudio);
+    }
+
+    tecla.onkeydown = function (evento) {
+
+        if (evento.code === 'Space' || evento.code === 'Enter') {
+            tecla.classList.add('ativa');
+        }
+
+    }
+
+    tecla.onkeyup = function () {
+        tecla.classList.remove('ativa');
+    }
+
 }
-const listaDeTeclas= document queryselector All('tecla')
-for(let contador= O;contador <listaDeTeclas length;contador++) {
-  const tecla= listaDeTeclas[contador]
-  const instrumento= teclaclasslist[3]
-  const Audio= '#som_${instrumento}';
-  tecla onclick= function(){
-    tocasom (;Audio);
-  }
